e536f641327f2abb3d4a58ba7a36fb1c8b1ce4ae
Mokshagna Malineni <mokshagna.malineni@accordion.com>
2026-07-20 10:25:49 +0000

    included(subcategory)

--- models/reporting_views/view_total_sales_by_product_category.sql
+++ models/reporting_views/view_total_sales_by_product_category.sql
@@ -3,9 +3,9 @@
     schema='REPORT_VIEWS'
 ) }}
 
-
 SELECT
     p.category,
+    p.subcategory,
     SUM(f.quantity_sold) AS total_quantity_sold,
     SUM(f.total_sales_amount) AS total_sales,
     SUM(f.profit_amount) AS total_profit
@@ -13,4 +13,5 @@ FROM {{ ref('fact_sales') }} f
 JOIN {{ ref('dim_product') }} p
     ON f.product_key = p.product_key
 GROUP BY
-    p.category
+    p.category,
+    p.subcategory
